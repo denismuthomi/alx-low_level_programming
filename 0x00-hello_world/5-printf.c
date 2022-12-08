82308ce9ffffa03a7637e6867b15b5c510611156
denismuthomi <mugunadenis95@gmail.com>
2022-12-08 10:38:36 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-this is the programs entry point
+ *
+ * Description:c program thats prints using printf
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
