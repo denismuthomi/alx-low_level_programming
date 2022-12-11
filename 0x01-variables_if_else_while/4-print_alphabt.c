9c50b16a81d2a5d4516e9a3e39217a73db3b42c4
denismuthomi <mugunadenis95@gmail.com>
2022-12-11 03:44:20 -0800

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Description: print a-z with the exception of q and e
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+
+	{
+	if (alp == 'e' || alp == 'q'
+		++alp;
+	putchar(alp);
+	++alp;
+	}
+	putchar('\n')
+	return (0);
