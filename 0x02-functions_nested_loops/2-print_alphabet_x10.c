3192720a642892b6dfea4373347f345360ea3b84
denismuthomi <mugunadenis95@gmail.com>
2022-12-14 04:18:43 -0800

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print-alphabet_x10 - print the alphabet 10 times
- */
+ * print_alphabet_x10 - prints the alphabet 10 times
+*/
 void print_alphabet_x10(void)
 {
 	int alp, l;
