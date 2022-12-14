59bed44e1ea28b840442e2e9de0df586172f7691
denismuthomi <mugunadenis95@gmail.com>
2022-12-14 07:01:35 -0800

    4 is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _islower - checks for lowercase character
+ * _isalpha - checks if a variable is an alphabet
  *
- * @c: the  variable to be checked
+ * @c: the variable to be checked
  *
- * Return: return 1 if the variable is lowercase and 0 if it isn't
+ * Return: 1 if true 0 if otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	return (0);
 }
